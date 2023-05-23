119aa16000d9347f1c224c6c6977850d682ab412
Timothy Callow <timjcallow@gmail.com>
2023-05-23 15:12:12 +0200

    Improve auto cif file generation

--- malada/providers/supercell.py
+++ malada/providers/supercell.py
@@ -38,8 +38,14 @@ class SuperCellProvider(Provider):
         cif_file : string
             Path to cif file used for supercell creation.
         """
-        file_name = self.parameters.element + "_" + str(self.parameters.number_of_atoms) \
-                    + "_" + self.parameters.crystal_structure + ".vasp"
+        file_name = (
+            self.parameters.element
+            + "_"
+            + str(self.parameters.number_of_atoms)
+            + "_"
+            + self.parameters.crystal_structure
+            + ".vasp"
+        )
         self.supercell_file = os.path.join(provider_path, file_name)
         if self.external_supercell_file is None:
             try:
@@ -47,18 +53,20 @@ class SuperCellProvider(Provider):
             except FileNotFoundError:
                 self.generate_cif(cif_file)
                 primitive_cell = ase.io.read(cif_file, format="cif")
-            transformation_matrix = structure_to_transformation\
-                                    [self.parameters.crystal_structure]\
-                                    [self.parameters.number_of_atoms]
+            transformation_matrix = structure_to_transformation[
+                self.parameters.crystal_structure
+            ][self.parameters.number_of_atoms]
 
-            super_cell = ase.build.make_supercell(primitive_cell,
-                                                  transformation_matrix)
-            super_cell = self.get_compressed_cell(super_cell,
-                                                  stretch_factor = self.parameters.stretch_factor,
-                                                  density = self.parameters.mass_density,
-                                                  radius = self.parameters.WS_radius)
-            ase.io.write(self.supercell_file,
-                         super_cell, format="vasp", long_format=True)
+            super_cell = ase.build.make_supercell(primitive_cell, transformation_matrix)
+            super_cell = self.get_compressed_cell(
+                super_cell,
+                stretch_factor=self.parameters.stretch_factor,
+                density=self.parameters.mass_density,
+                radius=self.parameters.WS_radius,
+            )
+            ase.io.write(
+                self.supercell_file, super_cell, format="vasp", long_format=True
+            )
         else:
             copyfile(self.external_supercell_file, self.supercell_file)
             print("Getting <<supercell>>.vasp file from disc.")
@@ -71,15 +79,16 @@ class SuperCellProvider(Provider):
         radius=None,
         units_density="g/(cm^3)",
     ):
-
         if sum([stretch_factor is None, density is None, radius is None]) == 3:
             raise ValueError(
                 "At least one of stretch_factor, density and radius must be speficied"
             )
         elif sum([stretch_factor is None, density is None, radius is None]) < 2:
-            print("Warning: More than one of stretch factor, density, "
-                  "and radius is specified.\nRadius takes first priority, "
-                  "then density, finally stretch factor.")
+            print(
+                "Warning: More than one of stretch factor, density, "
+                "and radius is specified.\nRadius takes first priority, "
+                "then density, finally stretch factor."
+            )
         if density is not None:
             density_ambient = SuperCellProvider.get_mass_density(
                 supercell, unit=units_density
@@ -100,20 +109,21 @@ class SuperCellProvider(Provider):
         nr_electrons = 0
         for i in range(0, nr_atoms):
             nr_electrons += supercell[i].number
-        number_density = nr_electrons/volume_atoms
+        number_density = nr_electrons / volume_atoms
         angstrom3_in_m3 = 1 / (m * m * m)
         angstrom3_in_cm3 = angstrom3_in_m3 * 1000000
         if unit == "Angstrom^3":
             return number_density
         elif unit == "cm^3":
-            return number_density/angstrom3_in_cm3
+            return number_density / angstrom3_in_cm3
         else:
             raise Exception("Unit not implemented")
 
     @staticmethod
     def get_wigner_seitz_radius(supercell):
-        number_density = SuperCellProvider.\
-            get_number_density(supercell, unit="Angstrom^3")
+        number_density = SuperCellProvider.get_number_density(
+            supercell, unit="Angstrom^3"
+        )
         rs_angstrom = (3 / (4 * np.pi * number_density)) ** (1 / 3)
         return rs_angstrom / Bohr
 
@@ -127,9 +137,9 @@ class SuperCellProvider(Provider):
         u_angstrom3_in_kg_m3 = kg / (m * m * m)
         u_angstrom3_in_g_cm3 = u_angstrom3_in_kg_m3 * 1000
         if unit == "kg_m^3":
-            return mass_density/u_angstrom3_in_kg_m3
+            return mass_density / u_angstrom3_in_kg_m3
         elif unit == "g/(cm^3)":
-            return mass_density/u_angstrom3_in_g_cm3
+            return mass_density / u_angstrom3_in_g_cm3
         elif unit == "u_Angstrom^3":
             return mass_density
         else:
@@ -137,16 +147,57 @@ class SuperCellProvider(Provider):
 
     def generate_cif(self, cif_file):
         # read in the api key
-        with open(self.parameters.mp_api_file, "r") as f:
-            api_key = f.readlines()[0].strip()
+        try:
+            with open(self.parameters.mp_api_file, "r") as f:
+                api_key = f.readlines()[0].strip()
+        except FileNotFoundError:
+            raise Exception(
+                "File containing materials project API key not found.\n"
+                "If you want to generate structures automatically, please "
+                "register an account on the Materials Project website.\n"
+                "Then save the API key in a file and set the file location "
+                "using parameters.mp_api_file."
+            )
         # initialize materials project api
         mpr = MPRester(api_key)
-        # dictionary relating elements to their MP IDs
-        # TODO: maybe this needs to go elsewhere
-        mat_ids = {"Al": "mp-134", "Be": "mp-87"}
-        structure = mpr.get_structure_by_material_id(mat_ids[self.parameters.element], conventional_unit_cell=True)
-        structure.to(filename = cif_file)
-        
-        
-
-
+        # dictionary relating crystal structures to their space groups
+        # TODO should this be moved elsewhere?
+        lattice_space_groups = {"fcc": 225, "bcc": 229, "hcp": 194}
+        # search materials project database for the desired element
+        structures = mpr.summary.search(
+            chemsys=self.parameters.element,
+            fields=["symmetry", "material_id", "theoretical", "energy_above_hull"],
+        )
+        # narrow down by correct structure
+        structures = list(
+            filter(
+                lambda x: x.symmetry.number
+                == lattice_space_groups[self.parameters.crystal_structure],
+                structures,
+            )
+        )
+        if len(structures) == 0:
+            raise Exception(
+                "No structure found for element "
+                + self.parameters.element
+                + " with crystal structure"
+                + self.parameters.crystal_structure
+                + "\n."
+                + "Please reconsider parameters or provide your own input files."
+            )
+        # narrow down by structures which are experimentally verified
+        structures_expt = list(filter(lambda x: x.theoretical == False, structures))
+        # choose minimal energy structure
+        if len(structures_expt) == 0:
+            best_structure = min(structures, key=lambda x: x.energy_above_hull)
+        elif len(structures_expt) == 1:
+            best_structure = structures_expt[0]
+        elif len(structures_expt) > 1:
+            best_structure = min(structures_expt, key=lambda x: x.energy_above_hull)
+        # get the ID of the best structure
+        best_structure_id = best_structure.material_id
+        structure = mpr.get_structure_by_material_id(
+            best_structure_id, conventional_unit_cell=True
+        )
+        # write to the cif file
+        structure.to(filename=cif_file)
